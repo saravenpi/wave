7707a26e594c553ae5ee26de3b3b88aab31a4b1e
Saravenpi Shigeturi <saravepi@tuta.io>
2024-07-02 16:13:08 +0200

    ♻️ added comments

--- src/request.c
+++ src/request.c
@@ -1,3 +1,4 @@
+
 #include "wave.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,25 +7,24 @@
 Request *parseRequest(const char *request)
 {
     Request *req = (Request *)malloc(sizeof(Request));
-
     if (!req) {
         perror("[WAVE] Failed to allocate memory for request");
         return NULL;
     }
+
+    // Parse request line
     char *requestLine = strtok(strdup(request), "\r\n");
     if (requestLine) {
-        char *method = strtok(requestLine, " ");
-        char *path = strtok(NULL, " ");
-        char *version = strtok(NULL, " ");
-        if (method && path && version) {
-            req->method = strdup(method);
-            req->path = strdup(path);
-            req->version = strdup(version);
-        }
+        req->method = strdup(strtok(requestLine, " "));
+        req->path = strdup(strtok(NULL, " "));
+        req->version = strdup(strtok(NULL, " "));
+        free(requestLine);
     } else {
         free(req);
         return NULL;
     }
+
+    // Parse headers
     char *headers = strstr(request, "\r\n");
     if (headers) {
         headers += 2;
@@ -38,18 +38,13 @@ Request *parseRequest(const char *request)
             }
         }
     }
-    if (req->headers && strlen(req->headers) == 0) {
-        free(req->headers);
-        req->headers = NULL;
-    }
+
+    // Parse body
     char *body = strstr(request, "\r\n\r\n");
     if (body) {
         body += 4;
         req->body = strdup(body);
     }
-    if (req->body && strlen(req->body) == 0) {
-        free(req->body);
-        req->body = NULL;
-    }
+
     return req;
 }
